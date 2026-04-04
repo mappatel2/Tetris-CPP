1af0101d7bc7d3aafc28df059dc4353914f3a0e1
Mohit Patel <mappatel2@gmail.com>
2026-04-04 11:14:04 -0400

    Added operator overloaded function of +=, << operators

--- Vector2Int.h
+++ Vector2Int.h
@@ -1,6 +1,9 @@
 #pragma once
 
+#include <iostream>
+
 namespace Tetris {
+
     struct Vector2Int {
         int x;
         int y;
@@ -24,16 +27,33 @@ namespace Tetris {
             this->y = other.y;
         }
 
-        Vector2Int operator+(const Vector2Int& rhs) const {
+        Vector2Int operator+(const Vector2Int rhs) const {
             return {x + rhs.x, y + rhs.y};
         }
 
-        Vector2Int operator-(const Vector2Int& rhs) const {
+        Vector2Int operator-(const Vector2Int rhs) const {
             return {x - rhs.x, y - rhs.y};
         }
 
-        Vector2Int operator* (const Vector2Int& rhs) const {
+        Vector2Int operator* (const Vector2Int rhs) const {
             return {x * rhs.x, y * rhs.y};
+        }
+
+        Vector2Int& operator+= (const Vector2Int rhs) {
+            x += rhs.x;
+            y += rhs.y;
+            return *this;
+        }
+
+        Vector2Int& operator*= (const int rhs) {
+            x *= rhs;
+            y *= rhs;
+            return *this;
+        }
+
+        friend std::ostream& operator<<(std::ostream& os, const Vector2Int& rhs) {
+            os << "[ " << rhs.x << ", " << rhs.y << " ]";
+            return os;
         }
     };
 }

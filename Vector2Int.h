b1ef2da647f1d53b742eb53994a5a5d7a5426e56
mappatel2 <mappatel2@gmail.com>
2026-04-05 17:25:53 -0400

    Added additional operator overloading functions in Vector2Int

--- Vector2Int.h
+++ Vector2Int.h
@@ -3,40 +3,25 @@
 #include <iostream>
 
 namespace Tetris {
-
     struct Vector2Int {
         int x;
         int y;
 
-        Vector2Int() {
+        constexpr Vector2Int() {
             x = y = 0;
         }
 
-        Vector2Int(const int x, const int y) :
+        constexpr Vector2Int(const int x, const int y) :
          x {x} , y{y} { }
 
-        Vector2Int(const Vector2Int& other) = default;
-
-        void Update(const int x, const int y) {
-            this->x = x;
-            this->y = y;
+        void Update(const int xParam, const int yParam) {
+            this->x = xParam;
+            this->y = yParam;
         }
 
         void Update(const Vector2Int& other) {
             this->x = other.x;
             this->y = other.y;
-        }
-
-        Vector2Int operator+(const Vector2Int rhs) const {
-            return {x + rhs.x, y + rhs.y};
-        }
-
-        Vector2Int operator-(const Vector2Int rhs) const {
-            return {x - rhs.x, y - rhs.y};
-        }
-
-        Vector2Int operator* (const Vector2Int rhs) const {
-            return {x * rhs.x, y * rhs.y};
         }
 
         Vector2Int& operator+= (const Vector2Int rhs) {
@@ -45,15 +30,39 @@ namespace Tetris {
             return *this;
         }
 
+        Vector2Int& operator-= (const Vector2Int rhs) {
+            x -= rhs.x;
+            y -= rhs.y;
+            return *this;
+        }
+
+        Vector2Int& operator*= (const Vector2Int rhs) {
+            x *= rhs.x;
+            y *= rhs.y;
+            return *this;
+        }
+
         Vector2Int& operator*= (const int rhs) {
             x *= rhs;
             y *= rhs;
             return *this;
         }
-
-        friend std::ostream& operator<<(std::ostream& os, const Vector2Int& rhs) {
-            os << "[ " << rhs.x << ", " << rhs.y << " ]";
-            return os;
-        }
     };
+
+    constexpr std::ostream& operator<<(std::ostream& os, const Vector2Int& rhs) {
+        os << "[ " << rhs.x << ", " << rhs.y << " ]";
+        return os;
+    }
+
+    constexpr Vector2Int operator+(Vector2Int lhs, const Vector2Int rhs) {
+        return lhs += rhs;
+    }
+
+    constexpr Vector2Int operator-(Vector2Int lhs, const Vector2Int rhs) {
+        return lhs -= rhs;
+    }
+
+    constexpr Vector2Int operator*(Vector2Int lhs, Vector2Int rhs) {
+        return lhs *= rhs;
+    }
 }
